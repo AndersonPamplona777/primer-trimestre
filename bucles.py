d68a930c25cd09651cbe55c9b880656db1c28f3a
AndersonPamplona777 <pamplonaanderson563@gmail.com>
2025-08-22 17:43:01 -0500

    Add files via upload

---
+++ bucles.py
@@ -0,0 +1,80 @@
+# num=int(input("ingrese un numero: "))
+# while num > 0:
+#     print(f"{num}")
+#     num -=1
+# print ("termino el conteo")
+
+
+#----
+
+# numero= int(input("ingrear numero dela tabla de multiplicar: ")) # pide al usuario un numero y lo convierte a entero 
+
+# i = 1 # inicializamos el contador en 1 (primer multiplicador)
+
+# print(f"\n inicia el contador en 1 {numero}: ") # imprime un titulo para la tabla de multiplicar
+
+# while i <= 10: # bucle que se repite mientras y sea menor a o igual 10 
+#     print(f"{numero} * {i} = {numero * i }") # muestra la multiplicacion y sus resultados
+#     i += 1 # incrementa i en 1 en cada interacion para avanzar en la tabla 
+
+
+#----
+
+# i = 1
+# while i < 6: 
+#     print(i)
+#     if i == 3:
+#         break
+#     i += 1
+
+#---
+
+# numero = 1 
+# while True:
+#     print(numero)
+#     numero += 1
+#     break 
+
+#--- 
+
+# x=5
+# while True:
+#     x -= 1 
+#     print(x)
+#     if x == 0:
+#         break
+# print("fin del bucle")
+
+
+#------------
+
+# chance= 1
+
+# while chance <= 3:
+#     txt = input ("escribe SI: ")
+#     if txt == "SI":
+#         print(" Ok, lo conseguiste en el intento", chance)
+#         break
+#     chance += 1
+# else:
+#     print("has agotado tus tres intentos")
+
+
+
+#-----------EJERCICIO 1 BUCLES------WHILE---
+
+# total = 0
+# while True:
+#     n1 = int(input("ingrese un numero para sumar(si quiere salir ingrese 0): "))
+#     if n1 == 0:
+#      break 
+#     n2 =int(input("ingrese el segundo valor(si quiere salir ingrese 0): "))
+
+#     total += n1 + n2
+
+# print(f"la suma total es {total}")
+
+    
+
+
+
